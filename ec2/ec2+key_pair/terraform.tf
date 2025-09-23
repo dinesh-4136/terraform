6b3fd692ecfeee7fdfec7f433e4259950ec85fe4
dinesh-4136 <126651499+dinesh-4136@users.noreply.github.com>
2025-09-23 22:59:10 +0530

    Update terraform.tf

--- ec2/ec2+key_pair/terraform.tf
+++ ec2/ec2+key_pair/terraform.tf
@@ -1,16 +1,16 @@
 terraform {
-	required_providers {
-		aws = {
-			source = "hashicorp/aws"
-			version = "~> 5.0"
-		}
-		tls = {
-			source = "hashicorp/tls"
-			version = "~> 4.0"
-		}
-		local = {
-			source = "hashicorp/local"
-			version = "~> 2.4"
-		}
-	}
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.4"
+    }
+  }
 }
